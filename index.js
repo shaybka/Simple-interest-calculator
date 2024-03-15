d561b1e130e68740b0e794492fc0d99e98d9d6a7
shaybka <adenmohmuud@gmail.com>
2024-03-15 17:59:08 +0300

    add functionality to our app

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 
 let principleInput = document.querySelector("#amount");
 let timeyInput = document.querySelector("#years");
-let timemInput = document.querySelector("#month");
 let rateInput = document.querySelector("#rate");
 let form = document.querySelector("form");
+let p=document.querySelector('#lcg');
+let total =document.querySelector('#principal');
+let interest=document.querySelector('#interest');
+
+// Function to calculate the interest
+const calculateInterest = ()=>{
+     // Convert values to integers
+ var principle = parseInt(principleInput.value);
+ var years = parseInt(timeyInput.value);
+ var rate = parseInt(rateInput.value)/100;
+    i=principle*rate*years;
+    p.innerHTML=`$${i}.00`;
+    t=principle+i;
+    total.innerHTML=`$${t}.00`;
+    interest.innerHTML=`$${i}.00`;
+
+}
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    principleInput=Number(principleInput.value);
-    console.log(principleInput);
+    calculateInterest();
+
+    
   
    
 });
